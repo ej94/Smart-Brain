5e0a7083c2ecf053b998d4866b756c2fe0e5d560
ej94 <amefawg66@gmail.com>
2019-03-02 10:55:01 +0800

    fix code

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component{
   }
 
   onSubmitSignIn=()=>{
-    fetch('http://localhost:3000/register',{
+    fetch('https://enigmatic-wildwood-68019.herokuapp.com/register',{
       method:'post',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({
